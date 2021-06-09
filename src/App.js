ffca40b643bdd1d483fc8d6466ee4656be14e478
Amal-Hamouda <82983648+Amal-Hamouda@users.noreply.github.com>
2021-06-09 13:48:33 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Form,Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Form className="Form">
+        <h1>JOIN US!!</h1>
+  <Form.Row>
+    <Form.Group controlId="formGridEmail">
+      
+      <Form.Control className="Formplaceboxes" type="email" placeholder="Enter email" />
+    </Form.Group>
+
+    <Form.Group  controlId="formGridPassword">
+      
+      <Form.Control className="Formplaceboxes" type="password" placeholder="Password" />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group controlId="formGridAddress1">
+    
+    <Form.Control className="Formplaceboxes"  placeholder="1234 Main St" />
+  </Form.Group>
+
+  <Form.Group controlId="formGridAddress2">
+    
+    <Form.Control className="Formplaceboxes" placeholder="Apartment, studio, or floor" />
+  </Form.Group>
+
+  <Form.Row>
+
+    <Form.Group  controlId="formGridState">
+      
+      <Form.Control className="Formplaceboxes"  as="select" defaultValue="Choose...">
+        <option>Choose...</option>
+        <option>Tunis</option>
+        <option>Sfax</option>
+        <option>Sousse</option>
+      </Form.Control>
+    </Form.Group>
+
+    <Form.Group  controlId="formGridZip">
+      
+      <Form.Control placeholder="ZIP" />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group id="formGridCheckbox">
+    <Form.Check className="Formplaceboxes" type="checkbox" label="Check me out" />
+  </Form.Group>
+
+  <Button id="submit-btn" className="Formplaceboxes"  variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
       </header>
     </div>
   );
